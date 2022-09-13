e3a96e758f8a1fcaa64ee9585889c6175f73ffb7
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-09-12 22:05:44 -0300

    se crea plantilla backend con dependencias jpa, mysql, spring devtools, web

---
+++ pymebackend/src/main/java/com/turismo/pyme/pymebackend/PymebackendApplication.java
@@ -0,0 +1,13 @@
+package com.turismo.pyme.pymebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymebackendApplication.class, args);
+	}
+
+}
---
+++ pymebackend/src/test/java/com/turismo/pyme/pymebackend/PymebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turismo.pyme.pymebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
